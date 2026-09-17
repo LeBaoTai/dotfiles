28d9f25d2fd189ed744d8b96057a49f5af0002b7
LeBaoTai <fnight644@gmail.com>
2026-09-17 17:52:28 +0700

    adding more config for telescope, using mini join and split

--- nvim/lua/config/mini.lua
+++ nvim/lua/config/mini.lua
@@ -2,9 +2,15 @@ require('mini.comment').setup()
 require('mini.pairs').setup()
 require('mini.ai').setup()
 require('mini.icons').setup()
+require('mini.splitjoin').setup({
+    mappings = {
+        toggle = 'gS',
+        split = '<leader>j',
+        join = 'J',
+    },
+})
 
 require('mini.files').setup()
 vim.keymap.set('n', '<leader>-', function()
     MiniFiles.open()
 end, { desc = 'Open MiniFiles' })
-
