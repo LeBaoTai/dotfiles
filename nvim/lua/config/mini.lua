c9b75cdec3083dc780f092681da41e3744c4248c
LeBaoTai <fnight644@gmail.com>
2026-09-18 11:45:19 +0700

    adding mini.cursorword

--- nvim/lua/config/mini.lua
+++ nvim/lua/config/mini.lua
@@ -2,6 +2,7 @@ require('mini.comment').setup()
 require('mini.pairs').setup()
 require('mini.ai').setup()
 require('mini.icons').setup()
+require('mini.cursorword').setup()
 require('mini.splitjoin').setup({
     mappings = {
         toggle = 'gS',
